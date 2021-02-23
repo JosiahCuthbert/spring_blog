8d01c35c8ab4624d4d0341eff68139fe70aacf1b
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2021-02-22 21:47:05 -0600

    relationships ex complete

---
+++ src/main/java/com/codeup/spring_blog/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.codeup.spring_blog.repositories;
+
+import com.codeup.spring_blog.models.Post;
+import com.codeup.spring_blog.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
