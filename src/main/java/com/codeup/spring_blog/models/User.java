cdb06e8c7563561308ab829ddd094fde553ba979
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2021-02-26 16:38:14 -0600

    security in progress

--- src/main/java/com/codeup/spring_blog/models/User.java
+++ src/main/java/com/codeup/spring_blog/models/User.java
@@ -24,6 +24,13 @@ public class User {
     @OneToMany(mappedBy = "user")
         private List<Post> posts;
 
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+
         public User(String username, String password,String email, long id){
             this.username = username;
             this.password = password;
